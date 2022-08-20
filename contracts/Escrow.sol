aa8d12e66fb90971a60e21e23c83a0b9047f57ce
Veselin Valkanov <veselin.d.valkanov@gmail.com>
2022-08-20 14:42:37 +0300

    Create some test validations for realEstate

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
 
-contract Escrow{
+interface IERC721{
+  function transferFrom(address _from,address _to,uint256 _id) external;
+}
 
+
+contract Escrow{
+  address public nftAddress;
+  uint256 public nftId;
+  address payable seller;
+  address payable buyer;
+
+  constructor (address _nftAddress,
+  uint256 _nftId,
+  address payable _seller,
+  address payable _buyer
+  ){
+    nftAddress = _nftAddress;
+    nftId = _nftId;
+    seller = _seller;
+    buyer =_buyer;
+  }
+
+  function finalizeSale() public{
+    IERC721(nftAddress).transferFrom(seller, buyer, nftId);
+  }
 }
